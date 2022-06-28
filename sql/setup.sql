054d5342c2e971469e1b341b91f86eb06313a66b
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-28 14:49:08 -0700

    table set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  task_name VARCHAR NOT NULL UNIQUE,
+  completed BOOLEAN NOT NULL,
+  user_id VARCHAR
+);
